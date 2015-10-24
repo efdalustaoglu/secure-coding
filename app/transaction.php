662129604db3fb45ed7975bf30d5bb618918b861
nikos <tsiamitros.nikos@gmail.com>
2015-10-24 21:56:47 +0200

    Filled in some transaction functions.

--- app/transaction.php
+++ app/transaction.php
@@ -5,50 +5,24 @@ require_once "db.php";
 // gets all transactions
 function getTransactions() {
   $transactions = selectTransactions();
-  if ($transactions->num_rows > 0) {
-    // output data of each row
-    echo "
-      <table>   
-        <tr>
-          <td>Transaction ID</td>
-          <td>Sender Account</td>
-          <td>Recipient Account</td>
-          <td>Amount</td>
-          <td>Status</td>
-          <td>TAN ID</td>
-          <td>Approved by</td>
-          <td>Date Approved</td>
-          <td>Date Created</td>
-        </tr>"
-    while($row = $transactions->fetch_assoc()) {
-      echo "
-        <tr>
-          <td>" . $row["ID"] . "</td>
-          <td>" . $row["SENDER_ACCOUNT"] . "</td>
-          <td>" . $row["RECIPIENT_ACCOUNT"]. "</td>
-          <td>" . $row["AMOUNT"]. "</td>
-          <td>" . $row["STATUS"]. "</td>
-          <td>" . $row["TAN_ID"]. "</td>
-          <td>" . $row["APPROVED_BY"]. "</td>
-          <td>" . $row["DATE_APPROVED"]. "</td>
-          <td>" . $row["DATE_CREATED"]. "</td>
-        </tr>
-      "; 
-    }
-    echo "</table>";
-  } else {
-    echo "";
-  }
+  return $transactions;
 }
 
 // gets a single transaction
 function getSingleTransaction($id) {
+  $transaction = selectTransaction();
+  return $transaction;
 
 }
 
 // creates a transaction
 function createTransaction($sender, $recipient, $amount, $tan) {
-
+  //TODO: check if parameters are valid
+  $res = insertTransaction($sender,$recipient,$amount,$tan);
+  if (!$res)
+    return 0;
+  else
+    return 1;
 }
 
 // approve / deny a transaction

a50e0fb56b7c678285dfd9754d82a10a4733f95c
nikos <tsiamitros.nikos@gmail.com>
2015-10-31 00:21:31 +0100

    changes to view transactions

--- public/view_transactions.php
+++ public/view_transactions.php
@@ -1,25 +1,19 @@
 <?php
-
 define('BANK_APP', TRUE);
-
 require_once "../app/user.php";
-require_once "../app/transactions.php";
-
-startSession();
-
-$transaction = getTransactions();
-
+require_once "../app/transaction.php";
+startSession(true);
+$transactions = getTransactions();
 // include header
 $pageTitle = "View Transactions";
 include("header.php");
-
 ?>
 
 <h3>View Transactions</h3>
 <p>
   <a class="pure-button pure-button-primary" href="create_transaction.php">New Transaction</a>
 </p>
-<table class="pure-table">
+<table class="pure-table pure-table-bordered">
   <thead>
     <tr>
       <th>#</th>
@@ -35,24 +29,26 @@ include("header.php");
     </tr>
   </thead>
   <tbody>
+  <?php foreach($transactions as $transaction): ?>
     <tr>
-      <td>1</td>
-      <td>Honda</td>
-      <td>Accord</td>
-      <td>2009</td>
-      <td>Accord</td>
-      <td>Accord</td>
-      <td>Accord</td>
-      <td>Accord</td>
-      <td>Accord</td>
-      <td><a href="">Open</a></td>
+      <td><?php echo $transaction->ID; ?></td>
+      <td><?php echo $transaction->DATE_CREATED; ?></td>
+      <td><?php echo $transaction->SENDER_ACCOUNT; ?></td>
+      <td><?php echo $transaction->RECIPIENT_ACCOUNT; ?></td>
+      <td><?php echo $transaction->AMOUNT; ?></td>
+      <td><?php if ($transaction->STATUS === "A") echo "Approved"; else if ($transaction->STATUS === "D") echo "Declined"; else echo "Pending"; ?></td>
+      <td><?php echo $transaction->TAN_ID; //TAN NUMBER ? ?></td>
+      <td><?php echo $transaction->APPROVED_BY; ?></td>
+      <td><?php echo $transaction->DATE_APPROVED; ?></td>
+      <td><a href="view_transaction.php?id=<?php echo $transaction->ID;?>">Open</a></td>
     </tr>
+  <?php endforeach; ?>
   </tbody>
 </table>
 
 <?php 
-
 // include footer
 include("footer.php"); 
-
 ?>
+
+
